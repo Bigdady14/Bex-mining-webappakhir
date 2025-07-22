405f36a5dd3a06241c0ea63936d645b9839256c6
Bigdady14 <bigexchangecripto@gmail.com>
2025-07-22 20:38:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 const wallet = "0xc3A9211b16c73A4f2C516b20658eC17c1D69d635";
 
 function mine() {
     document.getElementById("status").innerText = "Mining in progress...";
-    fetch('http://127.0.0.1:5000/mine', {
+    fetch('https://bd6caf04d2eb94c8c8b6954709f5c072.serveo.net/mine', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -21,7 +20,7 @@ function mine() {
 }
 
 function updateBalance() {
-    fetch('http://127.0.0.1:5000/balance?address=' + wallet)
+    fetch('https://bd6caf04d2eb94c8c8b6954709f5c072.serveo.net/balance?address=' + wallet)
     .then(res => res.json())
     .then(data => {
         document.getElementById("balance").innerText = "BEX Balance: " + data.balance;
